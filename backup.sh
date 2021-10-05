e80e2036334a1a77bca5b8a42862e2868361144b
viction <mason@viction.dev>
2021-10-05 18:37:02 +0100

    forgot a flag :D

--- backup.sh
+++ backup.sh
@@ -22,7 +22,7 @@ do
         --dbname $database > /tmp/dumps/$database.dump
 done
 
-tar cvzf $FILENAME .
+tar cvzf $FILENAME -C /tmp/dumps .
 
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
